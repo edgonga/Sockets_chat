f3c5951368a93292394abbd9dc26bc8273eb4849
Edu Gonzalez Garcia <johndoe@example.com>
2023-08-03 19:14:19 +0200

    go ahead

--- client/src/components/subcomponents/RoomSelectionModal.tsx
+++ client/src/components/subcomponents/RoomSelectionModal.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Modal from "react-modal";
+import "./RoomSelectionModal.css";
+import io from "socket.io-client"
 
 interface RoomSelectionModalProps {
   isOpen: boolean;
@@ -9,6 +11,29 @@ interface RoomSelectionModalProps {
 }
 
 const RoomSelectionModal: React.FC<RoomSelectionModalProps> = ({ isOpen, onRequestClose }) => {
+  const [existingRoom, setExistingRoom] = React.useState("");
+  const [newRoom, setNewRoom] = React.useState("");
+
+  const handleExistingRoomChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setExistingRoom(event.target.value);
+  };
+
+  const handleNewRoomChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewRoom(event.target.value);
+  };
+
+  const handleCreateRoom = () => {
+    if (newRoom.trim() !== "") {
+      const socket = io("http://localhost:5173")
+      socket.emit("newRoom", newRoom, (response: Response) => {
+        console.log("Server's response: ", response);
+        
+      })
+    }
+    console.log("New room created: ", newRoom);
+    onRequestClose(); 
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -16,8 +41,38 @@ const RoomSelectionModal: React.FC<RoomSelectionModalProps> = ({ isOpen, onReque
       contentLabel="Select or Create a Room"
       ariaHideApp={false}
     >
-      <h2>Select or Create a Room</h2>
-      {/* Agrega aquí el formulario o las opciones para seleccionar o crear una nueva sala */}
+      <div className="form-container">
+        <h2>Select or Create a Room</h2>
+        <div className="option">
+          <label htmlFor="existingRoom">Select Existing Room:</label>
+          <select
+            id="existingRoom"
+            value={existingRoom}
+            onChange={handleExistingRoomChange}
+          >
+            <option value="">Select a room</option>
+            <option value="room1">Room 1</option>
+            <option value="room2">Room 2</option>
+            {/* Agrega aquí más opciones para las rooms existentes */}
+          </select>
+        </div>
+        <div className="option">
+          <label htmlFor="newRoom">Create New Room:</label>
+          <input
+            type="text"
+            id="newRoom"
+            value={newRoom}
+            onChange={handleNewRoomChange}
+            placeholder="Enter new room name"
+          />
+        </div>
+        <div className="button-container">
+          <button onClick={onRequestClose}>Cancel</button>
+          <button onClick={handleCreateRoom} disabled={!newRoom}>
+            Create Room
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 };
